a7d6eb2ed59d8edb85be05c570d1ab23ecc9e06c
ED54000 <eloi_duchene@orange.fr>
2024-10-07 16:36:10 +0200

    Resolution des certaines erreurs

--- lib/Persistence/ServerSalt.php
+++ lib/Persistence/ServerSalt.php
@@ -3,11 +3,11 @@
  * PrivateBin
  *
  * a zero-knowledge paste bin
- *
- * @link      https://github.com/PrivateBin/PrivateBin
  * @copyright 2012 Sébastien SAUVAGE (sebsauvage.net)
+ * @link      https://github.com/PrivateBin/PrivateBin
  * @license   https://www.opensource.org/licenses/zlib-license.php The zlib/libpng License
  * @version   1.5.1
+ * @author    Sébastien SAUVAGE
  */
 
 namespace PrivateBin\Persistence;
